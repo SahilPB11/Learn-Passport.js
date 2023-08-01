2e14e1191c1f0a1b368c6b590de91bcada5967b4
Sahil <garg07825@gmail.com>
2023-08-01 11:04:41 +0530

    Added ejs & makes files in View folder & added some rotes in App.js

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ app.get('/', (req, res) => {
     res.render("index")
 })
 
+app.get('/login', (req, res) => {
+    res.render("login");
+})
+
+app.get('/register', (req, res) => {
+    res.render("register");
+})
+
+app.post('/register', (req, res) => {
+    
+})
+
 app.listen(3000, () => {
     console.log("Server is working on port number 3000");
 })
