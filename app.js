f3fc4fe964a33ae0c00c981cf74d5db780a677e2
Sahil <garg07825@gmail.com>
2023-08-01 11:47:37 +0530

    here e add data in views folder/files and Start using bcrypt for registeration

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from "express"
-
+import bcrypt from "bcrypt"
 const app = express();
+const users = [];
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:false}))
 
 app.get('/', (req, res) => {
     res.render("index")
@@ -10,13 +12,28 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.render("login");
 })
+app.post('/login', (req, res) => {
+    
+})
 
 app.get('/register', (req, res) => {
     res.render("register");
 })
 
-app.post('/register', (req, res) => {
-    
+app.post('/register', async(req, res) => {
+    const { name, email, password} = req.body;
+    try{
+        const hashedPassword = bcrypt.hashedPassword(password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: name,
+            email : email,
+            password : password
+        })
+        res.redirect('/')
+    }catch{
+        res.redirect("/register");
+    }
 })
 
 app.listen(3000, () => {
